eadc29f01999a5f862336ae776ed7cbe7f171917
Nurullah GUNGOR <ngngrx1@gmail.com>
2024-03-03 15:18:46 +0300

    blog.create sayfasının html kodu değiştirildi

--- resources/views/blog/create.blade.php
+++ resources/views/blog/create.blade.php
@@ -2,22 +2,25 @@
 
 @section('content')
     <div class="container">
-        <div class="card">
+        <div class="card mt-5">
+
             <div class="card-body">
                 <form>
                     <div class="mb-3">
-                      <label for="exampleInputEmail1" class="form-label">Email address</label>
-                      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                      <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                      <label for="" class="form-label">Category</label>
+                      <select name="" id="" class="form-control">
+                        <option>Tech</option>
+                      </select>
                     </div>
                     <div class="mb-3">
-                      <label for="exampleInputPassword1" class="form-label">Password</label>
-                      <input type="password" class="form-control" id="exampleInputPassword1">
+                      <label for="" class="form-label">Title</label>
+                      <input type="text" class="form-control" id="">
                     </div>
-                    <div class="mb-3 form-check">
-                      <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                      <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                    <div class="mb-3">
+                        <label for="" class="form-label">Body</label>
+                        <textarea name="" id="" rows="10" class="form-control"></textarea>
                     </div>
+
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </form>
             </div>
